ffedabb2586d4f8ab5322ac6fc14da28d3512066
twotanawin <tanawin.twox@gmail.com>
2024-05-17 23:33:59 +0700

    const var

--- website/index.js
+++ website/index.js
@@ -40,10 +40,21 @@
 
 // console.log(userName)
 
-let username;
+// let username;
+
+// document.getElementById("mySubmit").onclick = function(){
+//     username = document.getElementById("myText").value;
+//     // console.log(username)
+//     document.getElementById("myH1").textContent = `Hello ${username}`
+// };
+
+const PI = 3.1459;
+let radius;
+let circumference;
 
 document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("myText").value;
-    // console.log(username)
-    document.getElementById("myH1").textContent = `Hello ${username}`
+    radius = document.getElementById("myText").value; 
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById("myH3").textContent = circumference + "cm"
 };
